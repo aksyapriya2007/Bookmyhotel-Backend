f4a47a2c8a59b7fb47d53c54eaff4eb30bbf6b6a
aksyapriya2007 <aksyapraveen305@gmail.com>
2026-05-27 12:39:54 +0530

    Commit workspace changes

--- models/hotel.js
+++ models/hotel.js
@@ -1,67 +1,66 @@
 const mongoose = require('mongoose')
 
 const hotelSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,"hotel name is required"],
-        trim:true
+     name: {
+        type: String,
+        required: true,
+        trim: true
     },
-    city:{
-        type:String,
-        required:[true,"city is required for hotel"],
-        
+    type: {
+        // apartment villa , resort , cabin , cafeteria 
+        type: String,
+        required: [true, 'hotel type required']
     },
-    type:{
-        type:String,
-        required:[true,"hotel type is required"],
+    city: {
+        type: String,
+        required: [true, 'city is mandatory for hotel ']
     },
-    address:{
-        type:String,
-        required:[true,"address is required"],
+    // description:String,
+    ratings: {
+        type: Number,
+        min: 0,
+        max: 5
     },
-    distance:{
-        type:String,
-        required:[true,"distance is required"],
-    },
-    category:{
-        type:[String],
-        required:[true,"category is requird for customisation"]
-    },
-    images:{
-        type:[String],
-    },
-    rooms:{
-        type:[String],
-        required:[true,"rooms is require for hotel"],
-    },
-    cheapestPrice:{
-        type:Number,
-        required:[true,"price is required for a hotel"]
-    },
-    featured:{
-        type:Boolean,
-        default:false
-    },
-    description:{
-        type:String,
-        requird:[true,"description is required for this hotel"]
-    },
-    ratings:{
-        type:Number,
-        minlength:0,
-        maxlength:10,
-        required:[true,"ratings is required for hotel customisation"]
-    }
-})
+    address: {
+        type: String,
+        required: [true, 'address of hotel is required ']
 
+    },
+    distance: {
+        type: Number,
+        required: [true, 'current location to hotel location is required']
+    },
+    category: {
+        type: [String],
+        required: [true, 'category is required ']
+    },
+    images: {
+        type: [String]
+    },
+    rooms: {
+        type: [String]
+    },
+    cheapestPrice: {
+        type: Number,
+        required: [true, "Cheapest price is required for Hotel"]
+    },
+    feature: {
+        type: Boolean,
+        default: false
+    },
+    description: {
+        type: String,
+        required: [true, 'description is required ']
+    }
+
+})
  const Hotel = mongoose.model('Hotel', hotelSchema);
  const hotel1 = new Hotel({
 
             name: 'Dotel',
             city: 'Chennai',
-            price: 5000
+            cheapestPrice: 5000
      });
 
 // hotel1.save()
-
-module.exports = hotelSchema
+module.exports = Hotel
